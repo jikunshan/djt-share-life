053c277b79cbb7fecc61bc40ac6c2bc1e2c7fa5e
jikunshan <jikunshan@sohu.com>
2018-10-14 01:57:36 +0800

    jikunshan

--- djt-share-life-center/src/main/java/com/djt/share/life/center/interceptor/ShareLifeExceptionResolver.java
+++ djt-share-life-center/src/main/java/com/djt/share/life/center/interceptor/ShareLifeExceptionResolver.java
@@ -1,8 +1,11 @@
 package com.djt.share.life.center.interceptor;
 
+import com.alibaba.druid.support.logging.Log;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.djt.share.life.api.beans.core.Result;
 import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServletServerHttpResponse;
@@ -22,6 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class ShareLifeExceptionResolver implements HandlerExceptionResolver {
 
+	Log log = (Log) LogFactory.getLog(getClass());
 
     @Resource(name = "messageConverter")
     private FastJsonHttpMessageConverter messageConverter;
