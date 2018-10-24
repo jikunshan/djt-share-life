bfff07e5a31a04414b37f8646713157e0b2d147e
jks <jikunshan@sohu.com>
2018-10-25 00:03:05 +0800

    独立db，和dbclient工程

---
+++ djt-share-life-db-client/src/main/java/org/djt/share/life/db/client/App.java
@@ -0,0 +1,13 @@
+package org.djt.share.life.db.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ djt-share-life-db/src/main/java/org/djt/share/life/db/App.java
@@ -0,0 +1,13 @@
+package org.djt.share.life.db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
